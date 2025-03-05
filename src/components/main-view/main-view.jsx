aede73df8d4a28b228cba8ae75ff8ec219c7352e
Jonnits <jonathanrubra@gmail.com>
2025-03-04 16:40:08 -0800

    Created main-view, movie-view, and movie-card files and folders

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,63 @@
+
+import { MovieCard } from "../movie-card/movie-card";
+
+export const MainView = () => {
+    const [movies, setMovies] = useState([
+        {
+            id: 1,
+            title: "The Shawshank Redemption",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51zUbui+gbL._AC_.jpg",
+            description: "A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion.",
+            genre: "Drama",
+            director: "Frank Darabont",
+            year: 1994
+          },
+          {
+            id: 2,
+            title: "The Godfather",
+            image: "https://m.media-amazon.com/images/M/MV5BNGEwYjgwOGQtYjg5ZS00Njc1LTk2ZGEtM2QwZWQ2NjdhZTE5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+            description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+            genre: "Crime",
+            director: "Francis Ford Coppola",
+            year: 1972
+          },
+          {
+            id: 3,
+            title: "The Dark Knight",
+            image: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UX1000_.jpg",
+            description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+            genre: "Action",
+            director: "Christopher Nolan",
+            year: 2008
+          },
+          {
+            id: 4,
+            title: "The Godfather Part II",
+            image: "https://m.media-amazon.com/images/M/MV5BMDIxMzBlZDktZjMxNy00ZGI4LTgxNDEtYWRlNzRjMjJmOGQ1XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+            description: "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.",
+            genre: "Crime",
+            director: "Francis Ford Coppola",
+            year: 1974
+          },
+          {
+            id: 5,
+            title: "12 Angry Men",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/12_Angry_Men_%281957_film_poster%29.jpg/1200px-12_Angry_Men_%281957_film_poster%29.jpg",
+            description: "The jury in a New York City murder trial is frustrated by a single member whose skeptical caution forces them to more carefully consider the evidence before jumping to a hasty verdict.",
+            genre: "Drama",
+            director: "Sidney Lumet",
+            year: 1957
+          }
+    ]);
+    if (movies.length === 0) {
+        return <div>The list is empty!</div>;
+    }
+
+    return (
+      <div>
+        {movies.map((movie) => (
+            <MovieCard movie={movie} />
+        ))}
+      </div>
+    );
+  };
