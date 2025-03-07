a19d40282f5155a3c62294925057906188d55913
Jonnits <jonathanrubra@gmail.com>
2025-03-07 15:50:33 -0800

    Populated movies state using setMovies from API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useEffect(() => {
+        fetch("https://jonnits.github.io/MovieMinded/")
+        .then((response) => response.json())
+        .then((data) => {
+            const moviesFromApi = data.docs.map((doc) => {
+                return {
+                    id: doc.key,
+                    title: doc.title,
+                    image: doc.image,
+                    description: doc.description,
+                    genre: doc.genre,
+                    director: doc.director
+                };
+        });
+            setMovies(moviesFromApi);
+        });
+    }, []);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
