7b14ce0ad6b4954c9ec163a631d0250b0b54c2c0
Jonnits <jonathanrubra@gmail.com>
2025-03-27 14:36:22 -0700

    Updated MovieCard styling link

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { Button, Card } from "react-bootstrap";
 
-import "./movie-card.scss";
+import "./index.scss";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
